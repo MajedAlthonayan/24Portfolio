a927209f55a6acc9c3ec6d8002475a60de27e4de
MajedAlthonayan <majedalthonayan123@gmail.com>
2024-10-16 19:20:01 +0100

    style updates

--- 24portfolio/app/page.tsx
+++ 24portfolio/app/page.tsx
@@ -1,23 +1,54 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faFileAlt } from "@fortawesome/free-solid-svg-icons"; // Use solid icons
+
 export default function Home() {
 	return (
-		<main>
+		<main
+			style={{
+				fontFamily:
+					"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif",
+			}}
+		>
 			<div className="flex justify-center mt-20">
-				<div
-					className="text-left"
-					style={{ fontFamily: "'Playfair Display', serif" }}
-				>
-					<h1 className="text-5xl text-[#FFFFFF]">Hi, I'm</h1>
-					<h1 className="text-9xl text-[#BFA181]">Majed Althonayan</h1>
+				<div className="text-left">
+					<h1 className="text-5xl text-[#FFFFFF]">👋 Hi, I'm</h1>
+					<h1 className="text-9xl bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text text-transparent leading-[1.5]">
+						Majed Althonayan
+					</h1>
 
-					<div className="flex flex-col justify-center items-center bg-[#121212]">
-						<h1
-							className="text-center text-5xl text-[#A9A9A9] font-light mt-4"
-							style={{ fontFamily: "'Lato', sans-serif" }}
-						>
-							Developer | Security Engineer | ML Engineer
-						</h1>
-						<button className=" mt-6 w-1/4 px-10 py-2 bg-[#BFA181] text-[#111111] font-bold rounded hover:bg-[#a68d6b] shadow-lg hover:shadow-xl transition-shadow duration-200">
-							Contact Me
+					<div className="flex flex-row justify-center items-center bg-[#121212] space-x-4">
+						<button className="w-1/4 rounded-lg px-10 py-2 bg-[#FFFFFF] text-[#111111] font-bold shadow-lg hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200 flex items-center justify-center">
+							<FontAwesomeIcon
+								icon={faLinkedin}
+								className="w-6 h-6 mr-2"
+							/>{" "}
+							{/* LinkedIn Icon */}
+							LinkedIn
+						</button>
+						<button className="w-1/4 rounded-lg px-10 py-2 bg-[#FFFFFF] text-[#111111] font-bold shadow-lg hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200 flex items-center justify-center">
+							<FontAwesomeIcon
+								icon={faGithub}
+								className="w-6 h-6 mr-2"
+							/>{" "}
+							{/* GitHub Icon */}
+							GitHub
+						</button>
+						<button className="w-1/4 rounded-lg px-10 py-2 bg-[#FFFFFF] text-[#111111] font-bold shadow-lg hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200 flex items-center justify-center">
+							<FontAwesomeIcon
+								icon={faEnvelope}
+								className="w-6 h-6 mr-2"
+							/>{" "}
+							{/* Mail Icon */}
+							Mail
+						</button>
+						<button className="w-1/4 rounded-lg px-10 py-2 bg-[#FFFFFF] text-[#111111] font-bold shadow-lg hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200 flex items-center justify-center">
+							<FontAwesomeIcon
+								icon={faFileAlt}
+								className="w-6 h-6 mr-2"
+							/>{" "}
+							{/* Resume Icon */}
+							Resume
 						</button>
 					</div>
 				</div>
@@ -25,7 +56,7 @@ export default function Home() {
 			<div className="my-10 w-1/2 border-b border-[#FFFFFF] mx-auto border-2" />
 			<div className="flex justify-center">
 				<div className="w-1/2">
-					<p className="text-left text-xl text-[#FFFFFF] mt-4">
+					<p className="text-left text-xl text-[#FFFFFF] mt-4 ">
 						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
 						commodo ligula eget dolor. Aenean massa. Cum sociis natoque
 						penatibus et magnis dis parturient montes, nascetur ridiculus mus.
@@ -40,11 +71,8 @@ export default function Home() {
 			<div className="my-10 w-1/2 border-b border-[#FFFFFF] mx-auto border-2" />
 
 			<div className="flex justify-center mt-20">
-				<div
-					className="text-left"
-					style={{ fontFamily: "'Playfair Display', serif" }}
-				>
-					<h1 className="text-left text-5xl mb-10 text-[#BFA181]">
+				<div className="text-left">
+					<h1 className="text-left text-5xl mb-10 text-[#FFFFFF]">
 						Education & Certification
 					</h1>
 				</div>
@@ -52,11 +80,11 @@ export default function Home() {
 
 			<div className="w-1/2 mx-auto space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
 				<div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
-					<div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-[#BFA181] text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
+					<div className="flex items-center justify-center w-6 h-6 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-black text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
 
-					<div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] p-4 rounded border border-[#A9A9A9] shadow bg-[#A9A9A9] hover:bg-[#BDBDBD]">
+					<div className="rounded-lg w-[calc(100%-4rem)] md:w-[calc(50%-1.5rem)] p-4 rounded border border-[#FFFFFF] shadow bg-[#FFFFFF] hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200">
 						<div className="flex items-center justify-between space-x-2 mb-1">
-							<div className="font-bold text-slate-900">
+							<div className="font-bold text-black">
 								Secondary Education - GCSE's
 							</div>
 							<time className="font-caveat font-medium text-black">
@@ -73,9 +101,9 @@ export default function Home() {
 				{/* <!-- Item #2 --> */}
 				<div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
 					{/* <!-- Icon --> */}
-					<div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-[#BFA181] text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
+					<div className="flex items-center justify-center w-6 h-6 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-black text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
 					{/* <!-- Card --> */}
-					<div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow bg-[#A9A9A9]">
+					<div className="rounded-lg w-[calc(100%-4rem)] md:w-[calc(50%-1.5rem)] p-4 rounded border border-[#FFFFFF] shadow bg-[#FFFFFF] hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200">
 						<div className="flex items-center justify-between space-x-2 mb-1">
 							<div className="font-bold text-slate-900">
 								Secondary Education - A Level's
@@ -93,9 +121,9 @@ export default function Home() {
 				{/* <!-- Item #3 --> */}
 				<div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
 					{/* <!-- Icon --> */}
-					<div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-[#BFA181] text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
+					<div className="flex items-center justify-center w-6 h-6 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-black text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
 					{/* <!-- Card --> */}
-					<div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow bg-[#A9A9A9]">
+					<div className="rounded-lg w-[calc(100%-4rem)] md:w-[calc(50%-1.5rem)] p-4 rounded border border-[#FFFFFF] shadow bg-[#FFFFFF] hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200">
 						<div className="flex items-center justify-between space-x-2 mb-1">
 							<div className="font-bold text-slate-900">
 								Undergraduate Degree
@@ -104,6 +132,10 @@ export default function Home() {
 								2020-2023
 							</time>
 						</div>
+						<div className="font-bold text-slate-900">
+							Royal Holloway, University of London
+						</div>
+						<br />
 						<div className="text-slate-500">
 							Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
 							morbi blandit cursus risus.
@@ -114,9 +146,9 @@ export default function Home() {
 				{/* <!-- Item #4 --> */}
 				<div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
 					{/* <!-- Icon --> */}
-					<div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-[#BFA181] text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
+					<div className="flex items-center justify-center w-6 h-6 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-black text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
 					{/* <!-- Card --> */}
-					<div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow bg-[#A9A9A9]">
+					<div className="rounded-lg w-[calc(100%-4rem)] md:w-[calc(50%-1.5rem)] p-4 rounded border border-[#FFFFFF] shadow bg-[#FFFFFF] hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200">
 						<div className="flex items-center justify-between space-x-2 mb-1">
 							<div className="font-bold text-slate-900">
 								Postgraduate Degree
@@ -125,6 +157,10 @@ export default function Home() {
 								2023-2024
 							</time>
 						</div>
+						<div className="font-bold text-slate-900">
+							Imperial College London
+						</div>
+						<br />
 						<div className="text-slate-500">
 							Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
 							morbi blandit cursus risus.
@@ -135,9 +171,10 @@ export default function Home() {
 				{/* <!-- Item #5 --> */}
 				<div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
 					{/* <!-- Icon --> */}
-					<div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-[#BFA181] text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
+					<div className="flex items-center justify-center w-6 h-6 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-black text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"></div>
+
 					{/* <!-- Card --> */}
-					<div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow bg-[#A9A9A9]">
+					<div className="rounded-lg w-[calc(100%-4rem)] md:w-[calc(50%-1.5rem)] p-4 rounded border border-[#FFFFFF] shadow bg-[#FFFFFF] hover:scale-105 hover:shadow-xl transition-transform transition-shadow duration-200">
 						<div className="flex items-center justify-between space-x-2 mb-1">
 							<div className="font-bold text-slate-900">Certifications</div>
 							<time className="font-caveat font-medium text-black">2024</time>
